c50d8f6be780b0d4e51958f71040d718b936e193
M3579 <mihirkasmalkar@gmail.com>
2015-09-04 19:25:00 -0700

    Created an empty entry point for the program

---
+++ mypbasic.cpp
@@ -0,0 +1,27 @@
+/*
+ * mybasic.cpp
+ *
+ * /MyEasyPBASIC/Sources/src/mybasic.cpp
+ *
+ *       Author: Mihir Kasmalkar
+ * Date created: Sep 4, 2015
+ *
+ *      Purpose: The entry point for the compiler.
+ *
+ *        Usage:
+ *              $ mypbasic
+ *              Displays information about the MyEasyPBASIC compiler
+ *
+ *              $ mypbasic file
+ *              Compiles the given file
+ *
+ *        Notes:
+ *
+ */
+
+#define VERSION 0.1
+
+int main(int argc, const char* argv[])
+{
+
+}
